784ab9cc0f80b6c23ca413f01772c9bc4a781948
donaditoUVG <don22684@uvg.edu.gt>
2024-05-04 18:00:07 -0600

    sigue sin funcionar la compresión. Se intentó con el método de lectura readAllBytes. ;(

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import java.nio.file.*;
+
 public class Main {
     public static void main(String[] args) {
         String archivoEntrada = "C:/Users/josep/OneDrive - UVG/SEMESTRE III/Poo/HT11/HT11/lib/texto.txt"; //archivo de entrada
-        String archivoSalida = "salida.huff"; // Nombre del archivo compreso
+        String archivoSalida = "salida.huff"; // Nombre del archivo comprimido
         
+        try {
+            byte[] contenido = Files.readAllBytes(Paths.get(archivoEntrada));
+            String texto = new String(contenido);
+            System.out.println("Contenido del archivo:");
+            System.out.println(texto);
+        } catch (Exception e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+
         CompresorHuffman.comprimir(archivoEntrada, archivoSalida);
     }
 }
